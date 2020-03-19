c0342b4a46c943055a6c22d58f24744654eb9ef9
abdhala <abdhalahrodrigues@gmail.com>
2020-03-19 20:44:54 -0300

    Finalizada a aula: Fazendo cálculos com JavaScript.

---
+++ Fase02_IntroducaoAProgramacao/Aula4_FazendoCalculosComJavaScript.js
@@ -0,0 +1,11 @@
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia 
+// por mensagem o calculo da média dessas notas
+
+
+const aluno1 = "Abdhala", aluno2 = "Matheus", aluno3 = "Wendell"
+const notaAluno1 = 10, notaAluno2 = 9.8, notaAluno3 = 2
+
+const media = (notaAluno1+notaAluno2+notaAluno3)/3
+
+console.log("A média das notas dos alunos ",aluno1,", ", aluno2," e ",aluno3, " é: ",media,".")
