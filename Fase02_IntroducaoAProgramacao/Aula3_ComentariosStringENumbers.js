b58e258c182d40db59153fc638999b351dab7710
abdhala <abdhalahrodrigues@gmail.com>
2020-03-19 20:15:40 -0300

    Finalizada a aula: Comentários, Strings e Numbers.

---
+++ Fase02_IntroducaoAProgramacao/Aula3_ComentariosStringENumbers.js
@@ -0,0 +1,7 @@
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia 
+// por mensagem o calculo da média dessas notas
+
+
+const aluno1 = "Abdhala", aluno2 = "Matheus", aluno3 = "Wendell"
+const notaAluno1 = 10, notaAluno2 = 9.8, notaAluno3 = 2
