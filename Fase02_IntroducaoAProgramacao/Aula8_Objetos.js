7165aec8968d3c1aa159bd5a2b0edf57d16002e1
abdhala <abdhalahrodrigues@gmail.com>
2020-04-03 14:02:07 -0300

    Finalizada a aula: Objetos

---
+++ Fase02_IntroducaoAProgramacao/Aula8_Objetos.js
@@ -0,0 +1,21 @@
+// Aula de Objetos
+
+const aluno01 = {
+    nome: "Abdhala",
+    nota: 9.8
+}
+
+const aluno02 = {
+    nome: "Matheus",
+    nota: 10
+}
+
+const aluno03 = {
+    nome: "Samuel",
+    nota: 9.8
+
+}
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+console.log(media)
