0cc2132a955103635eb1dae8ca4641b2ce32bf79
abdhala <abdhalahrodrigues@gmail.com>
2020-04-03 15:20:28 -0300

    Finalizada a aula: Vetores

---
+++ Fase02_IntroducaoAProgramacao/Aula9_Vetores.js
@@ -0,0 +1,28 @@
+// Aula de Vetores
+
+const alunos = [
+    {
+    nome: "Abdhala",
+    nota: 9.8
+    }
+,
+    {
+    nome: "Matheus",
+    nota: 10
+    }
+,
+    {
+    nome: "Samuel",
+    nota: 9.8
+    }
+]
+
+const nomeDeAlunos = ["Abdhala", "Matheus", "Samuel"]
+
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+
+console.log(media)
+
+console.log(alunos);
+console.log(nomeDeAlunos);
+console.log(nomeDeAlunos[2]);
