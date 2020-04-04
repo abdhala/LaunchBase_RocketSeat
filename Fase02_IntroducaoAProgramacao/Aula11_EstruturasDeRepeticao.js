2e2429ab4e75bf38c4a997c508c767002fcb6056
abdhala <abdhalahrodrigues@gmail.com>
2020-04-03 23:40:57 -0300

    Finalizada a aula: Estrutura de Repetição.

---
+++ Fase02_IntroducaoAProgramacao/Aula11_EstruturasDeRepeticao.js
@@ -0,0 +1,60 @@
+// Criar um p
+
+const alunosTurmaA = [
+    {
+       nome: "Abdhala",
+       nota: 10
+    },
+    {
+       nome: "Diego",
+       nota: 9
+    },
+    {
+       nome: "Matheus",
+       nota: 8
+   },
+   {
+      nome: "Matheus",
+      nota: 8
+   }]
+
+const alunosTurmaB = [
+    {
+       nome: "João",
+       nota: 5
+    },
+    {
+       nome: "Pedro",
+       nota: 4
+   },
+   {
+       nome: "Gisele",
+       nota: 5
+   }]
+
+
+
+function calculaMedia(alunos){
+
+    let soma = 0;
+    for( let i = 0; i < alunos.length; i++){
+        
+        console.log(i);
+        soma = soma + alunos[i].nota;
+        console.log(soma);
+
+    };
+
+    const media = soma / alunos.length;
+
+    return media;
+}
+
+function imprimeMedia(){
+console.log(`A Média da Turma A é: ${calculaMedia(alunosTurmaA)}.`)
+console.log(`A Média da Turma B é: ${calculaMedia(alunosTurmaB)}.`)
+
+}
+
+
+imprimeMedia();
