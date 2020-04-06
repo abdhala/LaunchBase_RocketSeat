b2fe2dbc7459d752575c396a8de07ab3a6c50d03
abdhala <abdhalahrodrigues@gmail.com>
2020-04-06 07:31:19 -0300

    RETIFICAÇÃO: Finalizada a aula: Organização, padronização e escrita.

--- Fase02_IntroducaoAProgramacao/Aula14_OrganizacaoPadronizacaoEEscrita.js
+++ Fase02_IntroducaoAProgramacao/Aula14_OrganizacaoPadronizacaoEEscrita.js
@@ -1,15 +1,15 @@
 const alunosTurmaA = [
-    {
-       nome: "Abdhala",
-       nota: 10
-    },
-    {
-       nome: "Diego",
-       nota: 9
-    },
-    {
-       nome: "Matheus",
-       nota: 8
+   {
+      nome: "Abdhala",
+      nota: 10
+   },
+   {
+      nome: "Diego",
+      nota: 9
+   },
+   {
+      nome: "Matheus",
+      nota: 8
    },
    {
       nome: "Matheus",
@@ -17,40 +17,40 @@ const alunosTurmaA = [
    }]
 
 const alunosTurmaB = [
-    {
-       nome: "João",
-       nota: 5
-    },
-    {
-       nome: "Pedro",
-       nota: 4
+   {
+      nome: "João",
+      nota: 5
    },
    {
-       nome: "Gisele", 
-       nota: 5
+      nome: "Pedro",
+      nota: 4
+   },
+   {
+      nome: "Gisele",
+      nota: 5
    }]
 
 
 
-function calculaMedia(alunos, turma){
+function calculaMedia(alunos, turma) {
 
-    let soma = 0;
-    for( let i = 0; i < alunos.length; i++){
-        
-        console.log(i);
-        soma = soma + alunos[i].nota;
-        console.log(soma);
+   let soma = 0;
+   for (let i = 0; i < alunos.length; i++) {
 
-    };
+      console.log(i);
+      soma = soma + alunos[i].nota;
+      console.log(soma);
 
-    const media = soma / alunos.length;
+   };
 
-    return media, turma;
+   const media = soma / alunos.length;
+
+   return media, turma;
 }
 
-function imprimeMedia(){
-console.log(`A Média da Turma A é:  ${calculaMedia(alunosTurmaA)}.`)
-console.log(`A Média da Turma B é:  ${calculaMedia(alunosTurmaB)}.`)
+function imprimeMedia() {
+   console.log(`A Média da Turma A é:  ${calculaMedia(alunosTurmaA)}.`)
+   console.log(`A Média da Turma B é:  ${calculaMedia(alunosTurmaB)}.`)
 
 }
 
