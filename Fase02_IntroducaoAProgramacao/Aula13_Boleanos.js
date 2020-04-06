c042ad4f72ee179a6381a0ce37b5146a159d4f93
abdhala <abdhalahrodrigues@gmail.com>
2020-04-06 07:15:15 -0300

    Finalizada a aula: Boleanos.

---
+++ Fase02_IntroducaoAProgramacao/Aula13_Boleanos.js
@@ -0,0 +1,6 @@
+const aluno01 = "Abdhala", aluno02  = "Matheus", aluno03 = "Gustavo"
+const notaAluno01 = 9.8, notaAluno02 = 10, notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+console.log(media)
